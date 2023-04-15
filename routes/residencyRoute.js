2e1573e8f10209b899083e70e0929cdf73f5e62f
Hamza-pi <Hamza-pi>
2023-04-15 18:40:51 +0500

    schema updated

--- routes/residencyRoute.js
+++ routes/residencyRoute.js
@@ -15,7 +15,7 @@ router.post("/create", jwtCheck, createResidency);
 router.put("/update/:id", jwtCheck, updateResidency);
 router.delete("/delete/:id", jwtCheck, delResidency);
 router.put("/tofav/:rid", jwtCheck, toFav);
-router.get("/resd/:id", getResidency);
+router.get("/:id", getResidency);
 router.get("/allresd", getAllResidencies);
 
 export { router as residencyRoute };
